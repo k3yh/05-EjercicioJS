b3cfa84045dc7e30c45a1e28942210c6b1781f63
k3yh <115393708+k3yh@users.noreply.github.com>
2022-11-13 07:18:54 -0500

    Add files via upload

---
+++ 05-Ejercicio/Numeros.js
@@ -0,0 +1,10 @@
+let AlturaCM = 180
+let AlturaM = 1.8
+let PesoKG = 63.5
+
+console.log(AlturaM.toFixed(0))
+console.log(Math.floor(PesoKG))
+
+let maximo = Number.MAX_VALUE + 1 == Number.MAX_VALUE
+
+console.log(maximo)
